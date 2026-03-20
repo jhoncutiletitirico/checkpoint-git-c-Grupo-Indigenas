4c385509cac659e5115004094c0c1b94689b0a99
nobismo <zambelle1309@gmail.com>
2026-03-20 12:07:15 -0300

    Foi Adicionado Rodrigo Zambelle como aluno 3 no grupo

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(){
     printf("Grupo Indigenas");
     printf("Aluno 1: Jhon Cutile Titirico");
     printf("Aluno 2: Heitor Pena");
-    printf("Aluno 3: ");
+    printf("Aluno 3: Rodrigo Zambelle");
     printf("Aluno 4: ");
     return 0;
 }
