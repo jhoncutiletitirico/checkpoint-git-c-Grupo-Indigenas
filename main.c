d8fc68166fa61e312069ad9b157a216a5a7d2e44
nobismo <zambelle1309@gmail.com>
2026-03-20 12:17:57 -0300

    adicionei a idade no aluno Rodrigo Zambelle

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(){
     printf("Grupo Indigenas");
     printf("Aluno 1: Jhon Cutile Titirico");
     printf("Aluno 2: Heitor Pena");
-    printf("Aluno 3: Rodrigo Zambelle");
+    printf("Aluno 3: Rodrigo Zambelle, idade: 19");
     printf("Aluno 4: Angela Sousa Takezawa");
     return 0;
 }
