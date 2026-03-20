55111967e98b50d9d97718015e40b05a03e96176
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-20 12:22:29 -0300

    Idade

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
 
     printf("Grupo Indigenas");
     printf("Aluno 1: Jhon Cutile Titirico");
-    printf("Aluno 2: Heitor Pena");
+    printf("Aluno 2: Heitor Pena Gomes, 19 anos");
     printf("Aluno 3: Rodrigo Zambelle, idade: 19,genero: Homen");
     printf("Aluno 4: Angela Sousa Takezawa");
     return 0;
