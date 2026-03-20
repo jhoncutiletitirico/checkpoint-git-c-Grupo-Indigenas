3c098bc068e58dc323779ac9137680ca96aeab71
felipemello987 <felipemello200518@gmail.com>
2026-03-20 12:26:49 -0300

    foi adicionado a idade do felipe

--- main.c
+++ main.c
@@ -7,6 +7,6 @@ int main(){
     printf("Aluno 2: Heitor Pena Gomes, 19 anos, Homem");
     printf("Aluno 3: Rodrigo Zambelle, idade: 19,genero: Homen");
     printf("Aluno 4: Angela Sousa Takezawa");
-    printf("Aluno 5: Felipe Mello");
+    printf("Aluno 5: Felipe Mello, idade 20 anos");
     return 0;
 }
