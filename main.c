69375e6defe943c18788a8502e722c563e4e06ef
Heitor-Pena <heitorpenagomes@gmail.com>
2026-03-20 12:04:43 -0300

    Foi adicionado Heitor Pena como aluno 2 no grupo

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
 
     printf("Grupo Indigenas");
     printf("Aluno 1: Jhon Cutile Titirico");
-    printf("Aluno 2: ");
+    printf("Aluno 2: Heitor Pena");
     printf("Aluno 3: ");
     printf("Aluno 4: ");
     return 0;
