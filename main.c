e6ef8f81878940c45df5d2f0e03d3ad002c136ca
jhoncutiletitirico <jhoncutilegames@gmail.com>
2026-03-20 11:57:05 -0300

    alteração no codigo para abrir mais espaço para novos alunos ao grupo

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ int main(){
 
     printf("Grupo Indigenas");
     printf("Aluno 1: Jhon Cutile Titirico");
-
+    printf("Aluno 2: ");
+    printf("Aluno 3: ");
+    printf("Aluno 4: ");
     return 0;
 }
