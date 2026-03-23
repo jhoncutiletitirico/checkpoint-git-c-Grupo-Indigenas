e5ef6580b1a0b9c0cf3f74d2ca15142e81dd46e2
Heitor Pena <heitorpenagomes@gmail.com>
2026-03-22 23:08:54 -0300

    Teste02

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
 
     printf("Grupo Indigenas");
     printf("Aluno 1: Jhon Cutile Titirico");
-    printf("Aluno 2: Heitor Pena Gomes, 19 anos, Homem, Hétero");
+    printf("Aluno 2: Heitor Pena Gomes, 19 anos, Homem, Hétero, L");
     printf("Aluno 3: Rodrigo Zambelle, idade: 19,genero: Homen");
     printf("Aluno 4: Angela Sousa Takezawa");
     printf("Aluno 5: Felipe Mello, idade 20 anos, genero: Homem");
